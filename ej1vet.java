0f38f65d33145bb15367991d8ba2c14cfa8b67ba
Auto Logon <autologon@CSED.LAB>
2024-03-07 08:41:45 -0300

    java ej

---
+++ ej1vet.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ej1vet {
+
+    public static void main(String[] args) {
+    
+        Scanner leitor = new Scanner(System.in);
+
+        
+    
+    }
+
+}
